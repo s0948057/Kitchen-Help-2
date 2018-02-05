ba2bff1557d43145740a45aaf4744298b5b1c7ad
Miranda Richardson <s0948057@hh305-21ml56160.monmouth.edu>
2018-02-05 10:33:10 -0500

    Initial Commit

---
+++ Kitchen Help/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kitchen Help
+//
+//  Created by Miranda Richardson on 2/5/18.
+//  Copyright © 2018 Miranda Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
