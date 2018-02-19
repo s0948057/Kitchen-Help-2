d8e5274c7353142db719a56f0d6a74906c6f3238
Miranda Richardson <s0948057@hh305-21ml56160.monmouth.edu>
2018-02-19 10:53:57 -0500

    First push

---
+++ Kitchen Help/Controllers/SearchPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchPageViewController.swift
+//  Kitchen Help
+//
+//  Created by Miranda Richardson on 2/8/18.
+//  Copyright © 2018 Miranda Richardson. All rights reserved.
+//
+
+import UIKit
+
+class SearchPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
