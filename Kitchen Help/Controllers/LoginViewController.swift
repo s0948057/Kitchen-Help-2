c0b6034eaa4047570072c09b240b94c58c9d0b9a
Patrice K. Case <s0936878@hh305-20ml56162.monmouth.edu>
2018-03-27 13:17:42 -0400

    1) Fixed the error with Firebase. CocoaPods needs its pods installed on the computer you're working on. Go to the weekly report for steps on how to fix this. 2) Added the google sign in button so that it is an actual button. Still need to figure out why you can't click the button while on the computer.

--- Kitchen Help/Controllers/LoginViewController.swift
+++ Kitchen Help/Controllers/LoginViewController.swift
@@ -15,15 +15,38 @@ class LoginViewController: UITableViewController,  GIDSignInUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().signIn()
         
         // TODO(developer) Configure the sign-in button look/feel
         // ...
     }
+    //when the signin complets
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        //if any error stop and print the error
+        if error != nil{
+            print(error ?? "google error")
+            return
+        }
+        
+      
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+        if touch.view is GIDSignInButton {
+            return false
+        }
+        return true
+    }
+    func dismissKeyboard() {
+        self.view.endEditing(true)
     }
 
 
